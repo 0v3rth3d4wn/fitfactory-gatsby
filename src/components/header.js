4381bb9dc6fb3d3af74a9d21f32593fce281db76
Ivo Karadzohv <ivo.karadzhov@gmail.com>
2021-09-12 20:08:05 +0300

    Added react intersection observer and change header bgr

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useInView } from 'react-intersection-observer'
 import Nav from './nav'
 import Logo from './logo'
 
-const header = () => (
-  <header className="px-4 py-2 fixed top-0 left-0 right-0 w-full flex flex-wrap items-center justify-center z-40">
-    <Logo />
-    <Nav />
-  </header>
-)
+const Header = () => {
+  const topOffsetClass = 'top-[72px]'
+  const { ref, inView } = useInView()
 
-export default header
+  return (
+    <>
+      <header
+        className={`px-4 py-2 fixed top-0 left-0 right-0 w-full flex flex-wrap items-center justify-center z-40 ${
+          inView ? 'bg-transparent' : 'bg-black shadow'
+        } transition-colors duration-300`}
+      >
+        <Logo />
+        <Nav />
+      </header>
+      <div
+        ref={ref}
+        className={`absolute left-0 ${topOffsetClass} w-[1px] h-[1px] z-[-1]`}
+      />
+    </>
+  )
+}
+
+export default Header
