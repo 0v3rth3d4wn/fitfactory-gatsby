49fe42a63fa88fb38023ac8ae6462d83c84aa30a
Ivo Karadzohv <ivo.karadzhov@gmail.com>
2021-09-16 11:55:34 +0300

    Added business hours components

---
+++ src/components/business-hours.js
@@ -0,0 +1,78 @@
+import { StaticImage } from 'gatsby-plugin-image'
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import Separator from '../assets/images/separator.svg'
+import Button from './button'
+
+// Get business hours title and rangers
+const businessHoursQuery = graphql`
+  {
+    wpPage(isFrontPage: { eq: true }) {
+      businessHours {
+        businessHoursHeading
+        businessHoursLink {
+          target
+          title
+          url
+        }
+        ranges {
+          days
+          time
+        }
+      }
+    }
+  }
+`
+
+function BusinessHours() {
+  const {
+    wpPage: {
+      businessHours: { businessHoursHeading },
+    },
+    wpPage: {
+      businessHours: { ranges: businessHoursRanges },
+    },
+    wpPage: {
+      businessHours: { businessHoursLink },
+    },
+  } = useStaticQuery(businessHoursQuery)
+  return (
+    businessHoursRanges && (
+      <div className="relative overflow-hidden">
+        <StaticImage
+          alt="Black grid background"
+          className="absolute block top-0 left-0 w-full h-full"
+          layout="fullWidth"
+          src="../assets/images/black-grid-bgr.png"
+          quality="100"
+        />
+        <div className="px-4 py-12 relative z-30 flex flex-wrap items-center justify-center text-center">
+          <Separator className="mb-8" />
+          {businessHoursHeading && (
+            <h2 className="text-primary text-2xl text-center uppercase font-bold mb-6">
+              {businessHoursHeading}
+            </h2>
+          )}
+
+          {businessHoursRanges.map(range => (
+            <div className="mb-11">
+              <div className="text-base mb-2 uppercase text-white font-semibold">
+                {range.days}
+              </div>
+              <div className="text-gray font-bold text-5xl">{range.time}</div>
+            </div>
+          ))}
+
+          {businessHoursLink && (
+            <Button className="mb-8" to={businessHoursLink.url}>
+              {businessHoursLink.title}
+            </Button>
+          )}
+          <Separator className="rotate-180" />
+        </div>
+      </div>
+    )
+  )
+}
+
+export default BusinessHours
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../components/hero'
 import Features from '../components/features'
+import BusinessHours from '../components/business-hours'
 
 const index = () => (
   <div className="">
     <Hero />
     <Features />
+    <BusinessHours />
   </div>
 )
 
